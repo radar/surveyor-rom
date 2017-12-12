523e1cf4d2e1804aca615c99a9f071e5985bf6c9
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-12-12 11:31:12 +0100

    Boot rom as a component

--- lib/surveyor/application.rb
+++ lib/surveyor/application.rb
@@ -2,9 +2,30 @@ require 'dry/system/container'
 
 module Surveyor
   class Application < Dry::System::Container
+    boot(:rom, namespace: true) do |app|
+      init do
+        require "rom"
+
+        configuration = ROM::Configuration.new(:sql, 'postgres://localhost/surveyor_experiment')
+
+        register(:configuration, configuration)
+      end
+
+      start do
+        configuration = app['rom.configuration']
+
+        configuration.auto_registration(app.root.join('lib/surveyor'), namespace: true)
+        container = ROM.container(configuration)
+
+        register :container, container
+
+        require "surveyor/repos/survey_repo"
+        surveys_repo = Surveyor::Repos::SurveyRepo.new(container)
+
+        register :surveys_repo, surveys_repo
+      end
+    end
   end
 end
-
-require "surveyor/rom"
 
 Surveyor::Application.finalize!
